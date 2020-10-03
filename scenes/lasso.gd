349be96ab7c8d3dd03ed4a1e0cbf3c5f9c779b41
jmahon <joemahon89@gmail.com>
2020-10-03 13:24:53 +0100

    Added intial impulse for lasso

--- scenes/lasso.gd
+++ scenes/lasso.gd
@@ -11,42 +11,73 @@ var scale_speed = 0.5
 var inside_lasso = []
 onready var area2d_node = get_node("Area2D")
 onready var sprite_node = get_node("lasso_sprite")
+onready var rope_node = get_tree().get_root().get_node("Node2D/lasso_rope")
 
+var input_allowed = true
+var start_delay_timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	pass # Replace with function body.
+	set_lasso_scale(Vector2(0.5, 0.5))
+	pass
 
+func set_lasso_scale(scale):
+	area2d_node.scale.x = scale.x
+	area2d_node.scale.y = scale.y
+	sprite_node.scale.x = scale.x
+	sprite_node.scale.y = scale.y
+
+
+
+func shrink_lasso(delta):
+	area2d_node.scale.x -= scale_speed * delta
+	area2d_node.scale.y -= (scale_speed/2) * delta
+	sprite_node.scale.x -= scale_speed * delta
+	sprite_node.scale.y -= scale_speed * delta
+	rope_node.width = 3 * sprite_node.scale.x
+
+func grow_lasso(delta):
+	area2d_node.scale.x += scale_speed * delta
+	area2d_node.scale.y += (scale_speed/2) * delta
+	sprite_node.scale.x += scale_speed * delta
+	sprite_node.scale.y += scale_speed * delta
+	rope_node.width = 3 * sprite_node.scale.x
+
+
+func initial_impulse():
+	randomize()
+	var angle_x = (randi() % (10 - 0) + 0)*0.1*-1
+	randomize()
+	var angle_y = (randi() % (10 - 0) + 0)*0.1*-1
+	print(angle_x, angle_y)
+	var angle_vector = Vector2(angle_x, angle_y)
+	print(angle_vector)
+	apply_impulse(Vector2(), angle_vector * 250)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	if input_allowed == true:
 
-	if Input.is_action_pressed("lasso_up"):
-		var direction = Vector2(0,-1)
-		apply_impulse(Vector2(), direction * 1)
-	if Input.is_action_pressed("lasso_down"):
-		var direction = Vector2(0,1)
-		apply_impulse(Vector2(), direction * 1)
-	if Input.is_action_pressed("lasso_left"):
-		var direction = Vector2(-1,0)
-		apply_impulse(Vector2(), direction * 1)
-	if Input.is_action_pressed("lasso_right"):
-		var direction = Vector2(1,0)
-		apply_impulse(Vector2(), direction * 1)
+		if Input.is_action_pressed("lasso_up"):
+			var direction = Vector2(0,-1)
+			apply_impulse(Vector2(), direction * 1)
+		if Input.is_action_pressed("lasso_down"):
+			var direction = Vector2(0,1)
+			apply_impulse(Vector2(), direction * 1)
+		if Input.is_action_pressed("lasso_left"):
+			var direction = Vector2(-1,0)
+			apply_impulse(Vector2(), direction * 1)
+		if Input.is_action_pressed("lasso_right"):
+			var direction = Vector2(1,0)
+			apply_impulse(Vector2(), direction * 1)
+			
+		if Input.is_action_pressed("lasso_shrink"):
+			shrink_lasso(delta)
+			
+		if Input.is_action_pressed("lasso_grow"):
+			shrink_lasso(delta)
 		
-	if Input.is_action_pressed("lasso_shrink"):
-		area2d_node.scale.x -= scale_speed * delta
-		area2d_node.scale.y -= scale_speed * delta
-		sprite_node.scale.x -= scale_speed * delta
-		sprite_node.scale.y -= scale_speed * delta
-		
-		
-	if Input.is_action_pressed("lasso_grow"):
-		area2d_node.scale.x += scale_speed * delta
-		area2d_node.scale.y += scale_speed * delta
-		sprite_node.scale.x += scale_speed * delta
-		sprite_node.scale.y += scale_speed * delta
 		
 	#print(self.linear_velocity)
 
@@ -58,9 +89,10 @@ func _on_Area2D_area_entered(area):
 	print("IN", inside_lasso)
 	
 	var label_01 = get_tree().get_root().get_node("Node2D/Container/label_01")
-	label_01.text = ""
-	for creature in inside_lasso:
-		label_01.text = label_01.text + creature.get_name() + ","
+	if label_01:
+		label_01.text = ""
+		for creature in inside_lasso:
+			label_01.text = label_01.text + creature.get_name() + ","
 	
 
 func _on_Area2D_area_exited(area):
@@ -69,5 +101,6 @@ func _on_Area2D_area_exited(area):
 	
 	var label_01 = get_tree().get_root().get_node("Node2D/Container/label_01")
 	label_01.text = ""
-	for creature in inside_lasso:
-		label_01.text = label_01.text + creature.get_name() + ","
+	if label_01:
+		for creature in inside_lasso:
+			label_01.text = label_01.text + creature.get_name() + ","
--- scenes/main.gd
+++ scenes/main.gd
@@ -9,10 +9,28 @@ onready var lasso_node = get_node("YSort/lasso")
 onready var lasso_anchor_point_node = get_node("lasso_anchor_point")
 onready var lasso_rope_node = get_node("lasso_rope")
 
+var start_delay_timer
+var start_delay = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	lasso_node.input_allowed = false
 	OS.window_size = Vector2(720,720)
-	pass # Replace with function body.
+	
+	start_delay_timer = Timer.new()
+	start_delay_timer.set_wait_time(start_delay)
+	start_delay_timer.connect("timeout", self, "_on_start_delay_timer_timeout")
+	print("timer started")
+	add_child(start_delay_timer)
+	start_delay_timer.start()
+	
+func _on_start_delay_timer_timeout():
+	start_delay_timer.stop()
+	lasso_node.initial_impulse()
+	lasso_node.input_allowed = true
+
+
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
