e8fc7872dc7911efc24e49989d72178550ded14d
jmahon <joemahon89@gmail.com>
2020-10-03 10:35:50 +0100

    resizing of lasso

--- scenes/lasso.gd
+++ scenes/lasso.gd
@@ -7,9 +7,9 @@ extends RigidBody2D
 
 # Chnage damp (1 for speedy, 2 for slow)
 
-
-
-
+var scale_speed = 0.5
+onready var area2d_node = get_node("Area2D")
+onready var sprite_node = get_node("Sprite")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,7 +19,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(self.linear_velocity)
+
 	if Input.is_action_pressed("lasso_up"):
 		var direction = Vector2(0,-1)
 		apply_impulse(Vector2(), direction * 1)
@@ -32,3 +32,19 @@ func _process(delta):
 	if Input.is_action_pressed("lasso_right"):
 		var direction = Vector2(1,0)
 		apply_impulse(Vector2(), direction * 1)
+		
+	if Input.is_action_pressed("lasso_shrink"):
+		area2d_node.scale.x -= scale_speed * delta
+		area2d_node.scale.y -= scale_speed * delta
+		sprite_node.scale.x -= scale_speed * delta
+		sprite_node.scale.y -= scale_speed * delta
+		
+		
+	if Input.is_action_pressed("lasso_grow"):
+		area2d_node.scale.x += scale_speed * delta
+		area2d_node.scale.y += scale_speed * delta
+		sprite_node.scale.x += scale_speed * delta
+		sprite_node.scale.y += scale_speed * delta
+		
+		
+	print(self.linear_velocity)
