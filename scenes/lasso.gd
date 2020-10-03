320b2f1f56bd9dde69ef7655ced59d3e0263e8de
jmahon <joemahon89@gmail.com>
2020-10-03 14:17:05 +0100

    Added animation player and adjusted some speeds

--- scenes/lasso.gd
+++ scenes/lasso.gd
@@ -13,6 +13,10 @@ onready var area2d_node = get_node("Area2D")
 onready var sprite_node = get_node("lasso_sprite")
 onready var rope_node = get_tree().get_root().get_node("Node2D/lasso_rope")
 
+
+onready var animation_easy = get_node("animationplayer_easy")
+
+
 var input_allowed = true
 var start_delay_timer
 
@@ -46,13 +50,15 @@ func grow_lasso(delta):
 
 func initial_impulse():
 	randomize()
-	var angle_x = (randi() % (10 - 0) + 0)*0.1*-1
+	var angle_x = (randi() % (10 - 2) + 2)*0.1*-1
 	randomize()
-	var angle_y = (randi() % (10 - 0) + 0)*0.1*-1
+	var angle_y = (randi() % (10 - 2) + 2)*0.1*-1
 	print(angle_x, angle_y)
 	var angle_vector = Vector2(angle_x, angle_y)
 	print(angle_vector)
-	apply_impulse(Vector2(), angle_vector * 250)
+	apply_impulse(Vector2(), angle_vector * 300)
+	animation_easy.play("lasso_scale_anim")
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -61,16 +67,16 @@ func _process(delta):
 
 		if Input.is_action_pressed("lasso_up"):
 			var direction = Vector2(0,-1)
-			apply_impulse(Vector2(), direction * 1)
+			apply_impulse(Vector2(), direction * 2)
 		if Input.is_action_pressed("lasso_down"):
 			var direction = Vector2(0,1)
-			apply_impulse(Vector2(), direction * 1)
+			apply_impulse(Vector2(), direction * 2)
 		if Input.is_action_pressed("lasso_left"):
 			var direction = Vector2(-1,0)
-			apply_impulse(Vector2(), direction * 1)
+			apply_impulse(Vector2(), direction * 2)
 		if Input.is_action_pressed("lasso_right"):
 			var direction = Vector2(1,0)
-			apply_impulse(Vector2(), direction * 1)
+			apply_impulse(Vector2(), direction * 2)
 			
 		if Input.is_action_pressed("lasso_shrink"):
 			shrink_lasso(delta)
