4ded351d4b0cb8411c0652b0bd62d90061c948a8
jmahon <joemahon89@gmail.com>
2020-10-04 12:09:33 +0100

    basic menu screen

--- global.gd
+++ global.gd
@@ -1,15 +1,12 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 
 var current_captured = null
 
-onready var main_node = get_tree().get_root().get_node("Node2D")
-onready var score_counter = main_node.get_node("score_counter")
+var main_node
+var score_counter
+
 
 var level_01 = {"creature_01":40,
 				"creature_02":1}
@@ -23,17 +20,38 @@ var level = level_01
 var points = 0
 
 func add_points(points_to_add):
+	main_node = get_tree().get_root().get_node("Node2D")
+	score_counter = main_node.get_node("score_counter")
 	points += points_to_add
 	score_counter.set_text(str(points))
 	#score_counter.set_text("%05d" % points)
 	pass
 
 
-# Called when the node enters the scene tree for the first time.
+# Scene switching
+var current_scene = null
+
 func _ready():
-	pass # Replace with function body.
+	var root = get_tree().get_root()
+	current_scene = root.get_child(root.get_child_count() - 1)
+
+func goto_scene(path):
+	call_deferred("_deferred_goto_scene", path)
+
+func _deferred_goto_scene(path):
+	# It is now safe to remove the current scene
+	current_scene.free()
+
+	# Load the new scene.
+	var s = ResourceLoader.load(path)
+
+	# Instance the new scene.
+	current_scene = s.instance()
+
+	# Add it to the active scene, as child of root.
+	get_tree().get_root().add_child(current_scene)
+
+	# Optionally, to make it compatible with the SceneTree.change_scene() API.
+	get_tree().set_current_scene(current_scene)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
